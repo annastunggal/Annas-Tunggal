d58e02b5b119fe58de43671f99bf5e4efbe831f4
annastunggal <annastunggal4@gmail.com>
2018-09-07 08:14:13 -0700

    project pertama

---
+++ src/annastunggal/AnnasTunggal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package annastunggal;
+
+/**
+ *
+ * @author ANNAS
+ */
+public class AnnasTunggal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int n = 4;
+        int a,b;
+        for (a=0;a<n;a++){
+           for(b=0;b<a;b++){
+           System.out.print("*");
+           } 
+          System.out.println();  
+        }
+    }
+    }
+    
+
